1925cbbd8167a1776eb7e3554e70aabda177c6aa
mmy <ymm.812@gmail.com>
2015-09-23 09:16:24 +0800

    - 添加URL样式的显示

--- AttributeStringDemo/ViewController.m
+++ AttributeStringDemo/ViewController.m
@@ -8,7 +8,7 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()<UITableViewDataSource, UITableViewDelegate>
+@interface ViewController ()<UITableViewDataSource, UITableViewDelegate, UITextViewDelegate>
 
 @property (nonatomic, strong)   NSMutableArray *    dataSource;
 
@@ -43,7 +43,7 @@
         NSDictionary *  attributesDict;
         // 设置字体大小
         attributesDict  = @{
-                            NSFontAttributeName:    [UIFont systemFontOfSize:18]
+                            NSFontAttributeName:    [UIFont systemFontOfSize:30]
                             };
         rowDict         = @{
                             @"desc":    @"设置字体大小",
@@ -52,7 +52,7 @@
         [_dataSource addObject:rowDict];
         // 设置为斜体
         attributesDict  = @{
-                            NSObliquenessAttributeName: @(0.5)
+                            NSObliquenessAttributeName: @(1)
                             };
         rowDict         = @{
                             @"desc":    @"设置为斜体",
@@ -61,7 +61,8 @@
         [_dataSource addObject:rowDict];
         // 设置字体颜色
         attributesDict  = @{
-                            NSForegroundColorAttributeName: [UIColor redColor]
+                            NSForegroundColorAttributeName: [UIColor redColor],
+                            NSBackgroundColorAttributeName: [UIColor blackColor]
                             };
         rowDict         = @{
                             @"desc":    @"设置字体颜色",
@@ -70,8 +71,8 @@
         [_dataSource addObject:rowDict];
         // 设置删除线
         attributesDict  = @{
-                            NSStrikethroughStyleAttributeName:  @(1),
-                            NSStrikethroughColorAttributeName:  [UIColor redColor]
+                            NSStrikethroughStyleAttributeName:  @(5),
+                            NSStrikethroughColorAttributeName:  [UIColor yellowColor]
                             };
         rowDict         = @{
                             @"desc":    @"设置删除线",
@@ -80,7 +81,7 @@
         [_dataSource addObject:rowDict];
         // 设置下划线
         attributesDict  = @{
-                            NSUnderlineStyleAttributeName:  @(NSUnderlineStyleDouble),
+                            NSUnderlineStyleAttributeName:  @(NSUnderlineStyleSingle),
                             NSUnderlineColorAttributeName:  [UIColor redColor]
                             };
         rowDict         = @{
@@ -88,9 +89,28 @@
                             @"attr":    attributesDict
                             };
         [_dataSource addObject:rowDict];
+        // 设置URL
+        attributesDict  = @{
+                            NSLinkAttributeName:  @"点击我",
+                            };
+        rowDict         = @{
+                            @"desc":    @"设置URL链接",
+                            @"attr":    attributesDict
+                            };
+        [_dataSource addObject:rowDict];
+        // 设置URL可以点击
+        attributesDict  = @{
+                            NSLinkAttributeName:  @"http://www.baidu.com",
+                            };
+        rowDict         = @{
+                            @"desc":    @"设置URL链接",
+                            @"attr":    attributesDict,
+                            @"option":  @"UITextView"
+                            };
+        [_dataSource addObject:rowDict];
         // 设置行间距
         NSMutableParagraphStyle *   ps  = [[NSMutableParagraphStyle alloc] init];
-        ps.lineSpacing                  = 10;
+        ps.lineSpacing                  = 15;
         attributesDict  = @{
                             NSParagraphStyleAttributeName:  ps
                             };
@@ -113,7 +133,7 @@
                             NSFontAttributeName:    [UIFont systemFontOfSize:12],
                             NSKernAttributeName:    @(10)
                             };
-        rowDict         = @{@"desc":    @"计算文字所占区域 计算文字所占区域 计算文字所占区域 计算文字所占区域 计算文字所占区域 ",
+        rowDict         = @{@"desc":    @"计算文字所占区域 计算文字所占区域 计算文字所占区域 计算文字所占区域",
                             @"attr":    attributesDict,
                             @"option":  @"textSize"
                             };
@@ -136,9 +156,38 @@
         cell    = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:kCell];
         cell.textLabel.numberOfLines    = 0;
     }
-    NSAttributedString *    msg = [[NSAttributedString alloc] initWithString:self.dataSource[indexPath.row][@"desc"]
-                                                                  attributes:self.dataSource[indexPath.row][@"attr"]];
-    cell.textLabel.attributedText   = msg;
+    
+    if (self.dataSource[indexPath.row][@"option"]) {
+        NSString * option = self.dataSource[indexPath.row][@"option"];
+        if ([option isEqualToString:@"UITextView"]) {
+            for (UIView * subview in [cell.contentView subviews]) {
+                if([subview isKindOfClass:NSClassFromString(@"UITextView")]) {
+                    [subview removeFromSuperview];
+                }
+            }
+            UITextView * textView = [[UITextView alloc] initWithFrame:CGRectMake(0, 0, self.view.bounds.size.width, 88)];
+            textView.scrollEnabled = NO;
+            textView.editable = NO;
+            textView.textContainer.lineFragmentPadding = 0;
+            textView.textContainerInset = UIEdgeInsetsMake(0, 0, 0, 0);
+            textView.delegate = self;
+            textView.attributedText = [[NSAttributedString alloc] initWithString:self.dataSource[indexPath.row][@"desc"]
+                                                                      attributes:self.dataSource[indexPath.row][@"attr"]];
+            [cell.contentView addSubview:textView];
+        }
+        else {
+            NSMutableAttributedString *    msg = [[NSMutableAttributedString alloc] initWithString:self.dataSource[indexPath.row][@"desc"]
+                                                                          attributes:self.dataSource[indexPath.row][@"attr"]];
+            cell.textLabel.attributedText   = msg;
+
+        }
+    }
+    else {
+        NSAttributedString *    msg = [[NSAttributedString alloc] initWithString:self.dataSource[indexPath.row][@"desc"]
+                                                                      attributes:self.dataSource[indexPath.row][@"attr"]];
+        cell.textLabel.attributedText   = msg;
+
+    }
     return cell;
 }
 
@@ -164,6 +213,12 @@
                                                    otherButtonTitles:nil];
         [alertV show];
     }
+}
+
+#pragma mark - UITextView delegate
+
+- (BOOL)textView:(UITextView *)textView shouldInteractWithURL:(NSURL *)url inRange:(NSRange)characterRange {
+    return YES;
 }
 
 @end
